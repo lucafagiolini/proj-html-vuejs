631ee4e6d6bef321abf826836cddd421a12fa21d
Luca <lucafagiolini@live.it>
2024-03-11 17:57:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+
+export default {
+
+  components: {
+    AppNavbar,
+  },
+}
+
+</script>
+
+<template>
+  <div>
+    <AppNavbar></AppNavbar>
+  </div>
+  
+</template>
+
+<style>
+
+</style>
