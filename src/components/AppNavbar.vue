64fcbc221cac53549caf302b1151fcb1eb5a9c4f
Luca <lucafagiolini@live.it>
2024-03-12 01:56:27 +0100

    all buttons with hover are ready

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -3,18 +3,18 @@
 </script>
 
 <template>
-   <section class="container d-flex justify-content-between align-items-center">
+   <section class="container d-flex justify-content-between align-items-center py-3 px-0">
     <img id="logo_navbar" src="/src/assets/logo/construction_logo_retina.png" alt="avada_construction_logo">
-    <div class="d-flex">
-        <ul id="links_navbar">
-            <li>Home</li>
+    <div class="d-flex align-items-center">
+        <ul id="links_navbar" class="pe-3 roboto-regular text-uppercase">
+            <li class="ps-0">Home</li>
             <li>about</li>
             <li>services</li>
             <li>work</li>
             <li>articles</li>
         </ul>
 
-        <button>get quote</button>
+        <button id="nav_get_quote_btn" type="button" href="#" >get quote</button>
 
     </div>
 
@@ -22,6 +22,10 @@
 </template>
 
 <style lang="scss">
+@use "/src/variabiles.scss" as *;
+@use "/src/mixins.scss" as *;
+
+
 #logo_navbar {
   width: 230px;
   height: 80px;
@@ -29,10 +33,28 @@
 
 #links_navbar {
   display: flex;
-  list-style: none;
-  
   margin: 0;
+  font-size: 0.7rem;
+
+  list-style: none;
+  color: $light_gray_font_color;
+
+  li {
+    padding-right: 1rem;
+    padding-left: 1rem;
+    cursor: pointer;
+
+    &:hover {
+      color: $primary_color;
+    }
+  }
 }
+
+#nav_get_quote_btn {
+    @include primary_button_style;
+    font-size: 0.8rem;
+}
+
 
 
 
